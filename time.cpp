eea6b18377e56768a5ac67363aad938c227df2e8
aman-iitj <ug201310004@iitj.ac.in>
2016-11-15 15:56:41 +0530

    Added two Problems and Timing Code

---
+++ time.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	clock_t begin = clock();
+
+	// code_to_time();
+
+	clock_t end = clock();
+	double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+	cout << elapsed_secs << " " << endl;	
+}
