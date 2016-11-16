8e61aa7dae7a917bf09ea05ea3d658ae73f1d042
aman-iitj <ug201310004@iitj.ac.in>
2016-11-17 01:10:33 +0530

    Meet Half

---
+++ meet_middle.cpp
@@ -0,0 +1,132 @@
+/*
+Author : Aman Singh
+Solution for problem code meet_middle.
+ 
+==============================================
+A programmer started to cuss,
+'cause getting to sleep was a fuss.
+As he lay there awake in his bed,
+Looping around and 'round in his head,
+Was: while(!asleep()) sheep++;
+==============================================
+ 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vi; 
+typedef vector<vi> vvi; 
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+#define sz(a) int((a).size()) 
+// #define mp make_pair
+#define pb push_back 
+#define fi first
+#define se second
+#define buli(x) (__builtin_popcountll(x))
+#define all(c) (c).begin(),(c).end() 
+#define foreach(i, c) for(__typeof((c).begin()) i = (c).begin(); i != (c).end(); ++i)
+#define present(c,x) ((c).find(x) != (c).end()) 
+#define cpresent(c,x) (find(all(c),x) != (c).end()) 
+#define input(s, n) for(int i=0;i<n;i++)cin >> s[i];
+#define F(i, f, t) for(int i=f;i<t;i++)
+#define R(i,n) for(ll i=0;i<n;i++)
+#define cint(x) ll x; cin >> x;
+
+#define print(cnt,x) cout<<"Case #"<<cnt<<": "<<x<<"\n";
+
+template <class T>  inline void smax(T &x,T y){ x = max((x), (y));}
+template <class T>  inline void smin(T &x,T y){ x = min((x), (y));}
+
+#ifdef LOCAL
+#define fr freopen("in.txt","r",stdin)
+#define fo freopen("output.txt","w",stdout)
+
+#define error(x) cerr << #x << " = " << (x) <<endl
+#define Error(a,b) cerr<<"( "<<#a<<" , "<<#b<<" ) = ( "<<(a)<<" , "<<(b)<<" )\n";
+#define errop(a) cerr<<#a<<" = ( "<<((a).x)<<" , "<<((a).y)<<" )\n";
+#define dbg(x) F(i,0,sz(x)) cout << x[i] << " ";
+#define dbgg(x) F(i,0,sz(x)){F(j,0,sz(x[0])) cout << x[i][j] << " "; cout << endl;}
+#else
+#define fr 1
+#define fo 1
+#define error(x)
+#define Error(a,b)
+#define errop(a)
+#define dbg(x)
+#define dbgg(x)
+#endif
+
+inline void sc(int &x)
+{
+	bool f=0; x=0; char c=getchar();
+	while((c<'0' || c>'9') && c!='-') c=getchar();
+	if(c=='-') { f=1; c=getchar(); }
+	while(c>='0' && c<='9') { x=x*10+c-'0'; c=getchar(); }
+	if(f) x=-x; return;
+}
+
+void sc(int &x,int &y) { sc(x); return sc(y); }
+void sc(int &x,int &y,int &z) { sc(x); sc(y); return sc(z); }
+
+#define inf 1e9
+const double eps = 1e-7;
+
+ll rec(vi &arr, vector<ll> &res, ll curr, ll sum, int last){
+	if(curr == last) res.pb(sum);
+	else{
+		rec(arr, res, curr + 1, sum, last);
+		rec(arr, res, curr + 1, sum + arr[curr], last);
+	}
+}
+
+int solve(){
+
+	cint(n); cint(target);
+	vector<ll> arr;
+	R(i, n)
+
+	vector<ll> res, comp;
+	comp.insert(0);
+
+	if(n <= 20){
+		rec(arr, res, 0, 0, n);
+		sort(all(res));
+		ll x = lower_bound(all(res), target) - res.begin();
+		if(target == res[x]) return target;
+		else return res[x - 1];
+	}
+
+	else{
+		rec(arr, res, 0, 0, 20);
+		rec(arr, comp, 20, 0, n);	
+	}
+
+	// sort(all(res));
+	// for(auto x: res) cout << x << endl;	
+}
+
+int main(){
+	std::ios::sync_with_stdio(false);
+	fr;
+
+	clock_t begin = clock();
+
+	// code_to_time();
+
+
+	// cint(tt);
+	int tt = 1; 
+	F(tc, 1, tt + 1){
+		print(tc, solve());
+	}
+
+
+	clock_t end = clock();
+	double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+	cout << elapsed_secs << " " << endl;
+
+	return 0;
+}
